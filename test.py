978b543e759d589d5acff980bd027096a4f51178
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-02-17 11:02:06 -0500

    setup testing

--- test.py
+++ test.py
@@ -5,6 +5,21 @@ from boggle import Boggle
 
 
 class FlaskTests(TestCase):
+    def setUp(self):
+        """Setup prior to executing tests"""
 
-    # TODO -- write tests for every view function / feature!
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+    def test_homepage(self):
+        """Ensure information in session and HTML displaying properly"""
 
+        with self.client:
+            response = self.client.get('/')
+            self.assertIn('board', session)
+            self.assertIsNone(session.get('highscore'))
+            self.assertIsNone(session.get('nplays'))
+            self.assertIn(b'<p>High Score:', response.data)
+            self.assertIn(b'Score:', response.data)
+            self.assertIn(b'Seconds Left:', response.data)
+
+    
