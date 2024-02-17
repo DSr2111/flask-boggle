1959ee5f6f726559997dce3ae9ac47f3d90bc773
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-02-17 10:33:17 -0500

    homepage route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 from boggle import Boggle
 
 boggle_game = Boggle()
@@ -6,5 +6,19 @@ boggle_game = Boggle()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "mysecretkey"
 
+@app.route('/')
+def homepage():
+    """Initialize board"""
+    board = boggle_game.make_board()
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    
 
 
+@app.route('/')
+
+
+
+@app.route('/')
